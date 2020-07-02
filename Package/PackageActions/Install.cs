7fb273ce0edcbc0d0ed64b5493208e588a294560
angsnk0012 <melvin.ang@keysight.com>
2020-07-02 16:43:06 +0800

    Install to be always compatible

--- Package/PackageActions/Install.cs
+++ Package/PackageActions/Install.cs
@@ -90,7 +90,7 @@ namespace OpenTap.Package
             try
             {
                 // Get package information
-                List<PackageDef> packagesToInstall = PackageActionHelpers.GatherPackagesAndDependencyDefs(targetInstallation, PackageReferences, Packages, Version, Architecture, OS, repositories, Force, InstallDependencies, !Force, false);
+                List<PackageDef> packagesToInstall = PackageActionHelpers.GatherPackagesAndDependencyDefs(targetInstallation, PackageReferences, Packages, Version, Architecture, OS, repositories, Force, InstallDependencies, !Force, true);
                 if (packagesToInstall?.Any() != true)
                 {
                     log.Info("Could not find one or more packages.");
