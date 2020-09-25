407b8cd039090818532e5dcae974dfcf944c9250
angsnk0012 <melvin.ang@keysight.com>
2020-09-25 11:02:41 +0800

    Add example for Resource Open attribute

--- sdk/Examples/PluginDevelopment/TestSteps/Attributes/ResourceOpenAttributeExample.cs
+++ sdk/Examples/PluginDevelopment/TestSteps/Attributes/ResourceOpenAttributeExample.cs
@@ -7,75 +7,92 @@ using OpenTap;
 
 namespace OpenTap.Plugins.PluginDevelopment
 {
-    [Display("Resource Open Management Example",
+    [Display("Resource Open Before Example",
     Groups: new[] { "Examples", "Plugin Development", "Attributes" },
     Description: "TestStep that uses ResourceOpen attribute to show the different resource open modes.")]
-    public class ResourceOpenAttributeExample : TestStep
+    public class ResourceOpenBeforeAttributeExample : TestStep
     {
-        public ResourceOpenAttributeExample() { }
+        public ResourceOpenBeforeAttributeExample() { }
         public BaseInstrument BaseInstr { get; set; }
-
-        public override void Run()
-        {
-            BaseInstr.Open();
-        }
+        public override void Run() { }
     }
 
+    [Display("Resource Open Parallel Example",
+    Groups: new[] { "Examples", "Plugin Development", "Attributes" },
+    Description: "TestStep that uses ResourceOpen attribute to show the different resource open modes.")]
+    public class ResourceOpenParallelAttributeExample : TestStep
+    {
+        public ResourceOpenParallelAttributeExample() { }
+        public ParallelBaseInstrument BaseInstr { get; set; }
+
+        public override void Run() { }
+    }
+
+    // Subinstr is open before base instr is open, a sleep delay is used to show this sequence.
+    // Subinstr will close 2 sec after base instr is closed.
     public class BaseInstrument : Instrument
     {
         [ResourceOpen(ResourceOpenBehavior.Before)]
-        public NormInstr NormSubInstr { get; set; }
-
-        [ResourceOpen(ResourceOpenBehavior.InParallel)]
-        public ParallelInstr ParallelSubInstr { get; set; }
+        public Instrument SubInstr { get; set; }
 
         [ResourceOpen(ResourceOpenBehavior.Ignore)]
-        public IgnoreInstr IgnoreSubInstr { get; set; }
+        public Instrument IgnoreSubInstr { get; set; }
 
         public override void Open()
         {
+            TapThread.Sleep(2000);
+            Log.Info("Opening Base Instrument");
             base.Open();
             PrintSubInstrStatus();
         }
 
         public override void Close()
         {
+            TapThread.Sleep(1000);  // Can be some operations that take eg. 1 sec just to show it is connected
+
+            Log.Info("Closing Base Instrument");
+            base.Close();
+            TapThread.Sleep(2000);
+            PrintSubInstrStatus();
+        }
+
+        public void PrintSubInstrStatus()
+        {
+            Log.Info("NormSubInstr connected: {0}", SubInstr.IsConnected);
+            Log.Info("IgnoreSubInstr connected: {0}", IgnoreSubInstr.IsConnected);
+        }
+    }
+
+    // Subinstr will open in parallel with base instr. To show the difference in opening time, base instr is delayed by 2 secs.
+    // Subinstr will close as soon as connection is no longer needed. To show the sequence, base instr is delayed to close by 2 secs.
+    public class ParallelBaseInstrument : Instrument
+    {
+        [ResourceOpen(ResourceOpenBehavior.InParallel)]
+        public Instrument SubInstr { get; set; }
+
+        [ResourceOpen(ResourceOpenBehavior.Ignore)]
+        public Instrument IgnoreSubInstr { get; set; }
+
+        public override void Open()
+        {
+            TapThread.Sleep(2000);
+            Log.Info("Opening Base Instrument");
+            base.Open();
+            PrintSubInstrStatus();
+        }
+
+        public override void Close()
+        {
+            Log.Info("Closing Base Instrument");
+            TapThread.Sleep(2000);
             base.Close();
             PrintSubInstrStatus();
         }
 
         public void PrintSubInstrStatus()
         {
-            Log.Info("normSubInstr connected: {0}", NormSubInstr.IsConnected);
-            Log.Info("parallelSubInstr connected: {0}", ParallelSubInstr.IsConnected);
-            Log.Info("ignoreSubInstr connected: {0}", IgnoreSubInstr.IsConnected);
-        }
-    }
-
-    public class NormInstr : Instrument
-    {
-        public override void Open()
-        {
-            base.Open();
-            TapThread.Sleep(2000);
-        }
-    }
-
-    public class ParallelInstr : Instrument
-    {
-        public override void Open()
-        {
-            base.Open();
-            TapThread.Sleep(2000);
-        }
-    }
-
-    public class IgnoreInstr : Instrument
-    {
-        public override void Open()
-        {
-            base.Open();
-            TapThread.Sleep(2000);
+            Log.Info("ParallelSubInstr connected: {0}", SubInstr.IsConnected);
+            Log.Info("IgnoreSubInstr connected: {0}", IgnoreSubInstr.IsConnected);
         }
     }
 }
